b3c20b3e266d691d940f60f3752515189132b3fd
muhamadiqball <mi04111996@gmail.com>
2021-09-20 19:23:49 +0700

    upload file project latihan

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Muhamad Iqbal 
+ Saya tinggal di daerah Ciampea
+ Olahraga yang saya sukai adalah
+ Bermain Sepakbola dan bulu tangkis.";
+ }
+}
