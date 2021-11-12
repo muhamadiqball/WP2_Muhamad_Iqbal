77c0b7a004813650aa5407b14ab4ba241d4682f8
muhamadiqball <mi04111996@gmail.com>
2021-11-13 00:16:23 +0700

    Upload Tugas Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Muhamad</li>
+        <li>Nama Belakang : Iqbal</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jl. Cihideung ilir, Desa Cihideung ilir, Ciampea, Bogor</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Bogor</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Sepakbola</li>
+        <li>Bulutangkis</li>
+    </ul>
+</section>
